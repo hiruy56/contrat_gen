cc75c2841a3ac1efae82de5148f1e1cc3019f806
HIRUY METSIHAFE\DIO <75577876+hiruy56@users.noreply.github.com>
2024-07-04 13:52:08 +0300

    Update and rename main.py to api.py

--- api.py
+++ api.py
@@ -1,24 +1,27 @@
-import tempfile
-from fastapi import FastAPI, HTTPException, File, UploadFile
-from docx import Document
 import os
+from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from docx import Document
 from fastapi.responses import StreamingResponse
 from io import BytesIO
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+
 def fill_contract_template(contract_details):
     template_path = os.path.abspath('tomp.docx')
-
-    # Load the template document
     doc = Document(template_path)
-
-    # Replace placeholders with actual values
     for paragraph in doc.paragraphs:
         for key, value in contract_details.items():
             if f"{{{key}}}" in paragraph.text:
                 paragraph.text = paragraph.text.replace(f"{{{key}}}", str(value))
-
     for table in doc.tables:
         for row in table.rows:
             for cell in row.cells:
@@ -26,15 +29,16 @@ def fill_contract_template(contract_details):
                     for key, value in contract_details.items():
                         if f"{{{key}}}" in paragraph.text:
                             paragraph.text = paragraph.text.replace(f"{{{key}}}", str(value))
-
-    # Save the filled contract to a BytesIO stream
     stream = BytesIO()
     doc.save(stream)
     stream.seek(0)
-
     return stream
 
 @app.post("/fill-contract")
 async def fill_contract(contract_details: dict):
     filled_contract_stream = fill_contract_template(contract_details)
     return StreamingResponse(content=filled_contract_stream, media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
